31d031934065852080782d996636a3dd1749545c
nkepar <nikita@braceyourselfgames.com>
2022-12-09 13:04:43 -0800

    Setting up package.

---
+++ Runtime/Utility.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Project.Utility
+{
+	public class Utility
+	{
+		public void HelloWorld()
+		{
+			Debug.Log("Hello World");
+		}
+	}
+}
+
